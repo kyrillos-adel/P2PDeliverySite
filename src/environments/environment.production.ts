d070fc53b677a6a388d3595e17f3145ecac42eb1
kyrillos-adel <kyrillosadelfahim@gmail.com>
2025-05-03 05:22:35 +0300

    Update environment URLs to point to production server

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,9 +1,9 @@
 ﻿export const environment = {
   production: true,
-  apiBaseUrl: 'https://localhost:7176/',
-  chatHubUrl: 'https://localhost:7176/hub/chat',
-  imageBaseUrl: 'https://localhost:7176/images/',
-  notificationHubUrl: 'http://localhost:5153/hub/notification',
+  apiBaseUrl: 'http://p2pdelivery.runasp.net/',
+  chatHubUrl: 'http://p2pdelivery.runasp.net/hub/chat',
+  imageBaseUrl: 'http://p2pdelivery.runasp.net/images/',
+  notificationHubUrl: 'http://p2pdelivery.runasp.net/hub/notification',
   enableDebug: false,
   secure: false
 };
