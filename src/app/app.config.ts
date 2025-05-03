a19e7cb97f431b9f089b05cb64d26d6b7dd9d406
Kyrillos Adel <110562365+kyrillos-adel@users.noreply.github.com>
2025-05-03 20:22:58 +0300

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { routes, withHashLocation  } from './app.routes';
+import { provideRouter, withHashLocation } from '@angular/router';
+import { routes } from './app.routes';
 import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import {apiPrefixInterceptor} from './core/interceptors/api-prefix/api-prefix.interceptor';
 import {authorizationInterceptor} from './core/interceptors/authorization/authorization.interceptor';
