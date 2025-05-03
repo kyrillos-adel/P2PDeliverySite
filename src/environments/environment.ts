40d41f54a6e12d172aa90cc3e6c9e592b54a499d
Mina Michael <96148939+minamichael14@users.noreply.github.com>
2025-05-03 18:20:39 +0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,6 +8,6 @@ export const environment = {
   imageBaseUrl: 'http://p2pdelivery.runasp.net/images/',
   notificationHubUrl: 'http://p2pdelivery.runasp.net/hub/notification',
   secure: false,
-//   notificationHubUrl: 'http://localhost:5153/hub/notification',
+//   notificationHubUrl: 'https://localhost:5153/hub/notification',
   enableDebug: true
 };
